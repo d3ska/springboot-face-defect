310c9cf4d152560b94d6a727263159d24cacf6d6
Mateusz Deska <mateuszdeska00@gmail.com>
2020-11-19 00:13:58 +0100

    Initial commit

---
+++ src/main/java/pl/deska/springbootfacedefect/SpringbootFaceDefectApplication.java
@@ -0,0 +1,13 @@
+package pl.deska.springbootfacedefect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFaceDefectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFaceDefectApplication.class, args);
+    }
+
+}
