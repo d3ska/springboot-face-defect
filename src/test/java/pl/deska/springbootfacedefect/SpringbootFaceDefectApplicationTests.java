71335e2b0b820ee627677971aa2d112e8d502605
Mateusz Deska <mateuszdeska00@gmail.com>
2020-11-19 00:29:17 +0100

    add missing files

---
+++ src/test/java/pl/deska/springbootfacedefect/SpringbootFaceDefectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.deska.springbootfacedefect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFaceDefectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
